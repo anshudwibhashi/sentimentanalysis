ae40a050bb2e6c21d84f30f4d9d80bd32ddccfc6
Anshuman <anshudwibhashi@gmail.com>
2015-09-14 19:57:54 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -27,21 +27,18 @@ def classify(s):
     for token in tokens:
         prob_prod *= float((pos_words.count(token)+1))/(len(pos_words)+len(all_words))
     pos_prob = prob_prod * pos_prior
-    print prob_prod
 
     # For negative
     prob_prod = 1.0
     for token in tokens:
         prob_prod *= float((neg_words.count(token)+1))/(len(neg_words)+len(all_words))
     neg_prob = prob_prod * neg_prior
-    print prob_prod
 
     # For neutral
     prob_prod = 1.0
     for token in tokens:
         prob_prod *= float((neu_words.count(token)+1))/(len(neu_words)+len(all_words))
     neu_prob = prob_prod * neu_prior
-    print prob_prod
 
     return pos_prob, neg_prob, neu_prob
 
